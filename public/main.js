5abb4033dcd49ba53ef1f53d3192bc8c1de392cf
seongyunlee <priavate.seongyun@gmail.com>
2022-08-18 14:28:10 +0900

    hexa search refactor

--- public/main.js
+++ public/main.js
@@ -11,251 +11,64 @@ function onMouseOverTri(event) {
 function onMouseOutTri(event) {
   event.target.classList.remove("mouse-up");
 }
-function getUpperColIndex(row, col) {
-  if (row == 5) return col;
-  if (row > 5) return col + 1;
-  if (row < 5) return col - 1;
+function getUpperColDif(row) {
+  if (row == 5) return 0;
+  if (row > 5) return 1;
+  if (row < 5) return -1;
 }
-function getBottomColIndex(row, col) {
-  if (row == 4) return col;
-  if (row > 4) return col - 1;
-  if (row < 4) return col + 1;
+function getDownColDif(row) {
+  if (row == 4) return 0;
+  if (row > 4) return -1;
+  if (row < 4) return 1;
 }
 
-function checkUpperHexa(row, col) {
-  //upper Hexa
-  try {
-    if (all_tri[row][col].classList.contains("down")) return false;
-    const upperline = all_tri[row - 1].slice(col - 4, col);
-    if (
-      !(
-        all_tri[row][col - 1].classList.contains("owned") &&
-        all_tri[row][col + 1].classList.contains("owned")
-      )
-    ) {
-      return false;
-    }
-    for (var c = -1; c < 2; c++) {
+function checkHexa(row, col) {
+  //upper Hexa //the origin is lower center
+  if (row <= 0 || col >= all_tri[row].length - 1 || col < 0) return false;
+  if (all_tri[row][col].classList.contains("down")) return false;
+  for (var r = row - 1; r <= row; r++) {
+    for (var c = col - 1; c <= col + 1; c++) {
       if (
-        !all_tri[row - 1][getUpperColIndex(row, col) + c].classList.contains(
-          "owned"
-        )
+        !all_tri[r][
+          c + (r == row ? 0 : getUpperColDif(row))
+        ].classList.contains("owned")
       ) {
         return false;
       }
     }
-    return true;
-  } catch {
-    return false;
   }
+  return true;
 }
-function checkBottomLeftHexa(row, col) {
-  //left bottom Hexa.
-
-  try {
-    if (all_tri[row][col].classList.contains("down")) return false;
-
-    console.log(getBottomColIndex(row, col));
-    for (var c = 0; c < 3; c++) {
-      if (
-        !all_tri[row + 1][getBottomColIndex(row, col) - c].classList.contains(
-          "owned"
-        )
-      ) {
-        return false;
-      }
-    }
-    if (
-      !(
-        all_tri[row][col - 1].classList.contains("owned") &&
-        all_tri[row][col - 2].classList.contains("owned")
-      )
-    ) {
-      console.log("asdf");
-      return false;
-    }
-    return true;
-  } catch (e) {
-    console.log(e);
-    return false;
-  }
-}
-function checkBottomRightHexa(row, col) {
-  //right bottom Hexa
-  try {
-    if (all_tri[row][col].classList.contains("down")) return false;
-    for (var c = 0; c < 3; c++) {
-      if (
-        !all_tri[row + 1][getBottomColIndex(row, col) + c].classList.contains(
-          "owned"
-        )
-      ) {
-        return false;
-      }
-    }
-    if (
-      !(
-        all_tri[row][col + 1].classList.contains("owned") &&
-        all_tri[row][col + 2].classList.contains("owned")
-      )
-    ) {
-      return false;
-    }
-    return true;
-  } catch {
-    return false;
-  }
-}
-function checkBottomHexa(row, col) {
-  //right bottom Hexa
-  try {
-    if (all_tri[row][col].classList.contains("up")) return false;
-    const upperline = all_tri[row - 1].slice(col - 4, col);
-    for (var c = -1; c < 2; c++) {
-      if (!all_tri[row][col + c].classList.contains("owned")) {
-        return false;
-      }
-    }
-    for (var c = -1; c < 2; c++) {
-      if (
-        !all_tri[row - 1][getBottomColIndex(row, col) + c].classList.contains(
-          "owned"
-        )
-      ) {
-        return false;
-      }
-    }
-    return true;
-  } catch {
-    return false;
-  }
-}
-function offUpperLeftHexa(row, col) {
-  //upper left Hexa
-  try {
-    for (var c = 0; c < 3; c++) {
-      all_tri[row - 1][getUpperColIndex(row, col) - c].classList.remove(
+function offHexa(row, col) {
+  //upper Hexa //the origin is lower center
+  if (all_tri[row][col].classList.contains("down")) return false;
+  for (var r = row - 1; r <= row; r++) {
+    for (var c = col - 1; c <= col + 1; c++) {
+      all_tri[r][c + (r == row ? 0 : getUpperColDif(row))].classList.remove(
         "owned"
       );
     }
-    all_tri[row][col - 2].classList.remove("owned");
-    all_tri[row][col - 1].classList.remove("owned");
-  } catch {
-    return false;
-  }
-}
-function offUpperRightHexa(row, col) {
-  //upper left Hexa
-  try {
-    for (var c = 0; c < 3; c++) {
-      all_tri[row - 1][getUpperColIndex(row, col) + c].classList.remove(
-        "owned"
-      );
-    }
-    all_tri[row][col + 2].classList.remove("owned");
-    all_tri[row][col + 1].classList.remove("owned");
-  } catch {
-    return false;
-  }
-}
-function offUpperHexa(row, col) {
-  //upper left Hexa
-  try {
-    for (var c = -1; c < 2; c++) {
-      all_tri[row][col + c].classList.remove("owned");
-    }
-    for (var c = -1; c < 2; c++) {
-      all_tri[row - 1][getUpperColIndex(row, col) + c].classList.remove(
-        "owned"
-      );
-    }
-  } catch {
-    return false;
-  }
-}
-function offBottomRightHexa(row, col) {
-  //upper left Hexa
-  try {
-    for (var c = 0; c < 3; c++) {
-      all_tri[row + 1][getBottomColIndex(row, col) + c].classList.remove(
-        "owned"
-      );
-    }
-    all_tri[row][col + 2].classList.remove("owned");
-    all_tri[row][col + 1].classList.remove("owned");
-  } catch {
-    return false;
-  }
-}
-function offBottomLeftHexa(row, col) {
-  //upper left Hexa
-  try {
-    for (var c = 0; c < 3; c++) {
-      all_tri[row + 1][getBottomColIndex(row, col) - c].classList.remove(
-        "owned"
-      );
-    }
-    all_tri[row][col - 2].classList.remove("owned");
-    all_tri[row][col - 1].classList.remove("owned");
-  } catch {
-    return false;
-  }
-}
-function offBottomHexa(row, col) {
-  //upper left Hexa
-  try {
-    for (var c = -1; c < 2; c++) {
-      all_tri[row][col + c].classList.remove("owned");
-    }
-    for (var c = -1; c < 2; c++) {
-      all_tri[row + 1][getBottomColIndex(row, col) + c].classList.remove(
-        "owned"
-      );
-    }
-  } catch {
-    return false;
   }
 }
 function onClickTri(event) {
+  event.target.classList.add("owned");
   const [row, col] = event.target
     .getAttribute("value")
     .split(" ")
     .map((e) => {
       return parseInt(e);
     });
-  var toggled = false;
-  if (checkBottomLeftHexa(row, col)) {
-    console.log("bottom left on");
-    offBottomLeftHexa(row, col);
-    toggled = true;
-  }
-  if (checkBottomRightHexa(row, col)) {
-    console.log("bottom right on");
-    offBottomRightHexa(row, col);
-    toggled = true;
-  }
-  if (checkBottomHexa(row, col)) {
-    console.log("bottom on");
-    offBottomHexa(row, col);
-    toggled = true;
-  }
-  if (checkUpperRightHexa(row, col)) {
-    offUpperRightHexa(row, col);
-    console.log("upper right on");
-    toggled = true;
-  }
-  if (checkUpperLeftHexa(row, col)) {
-    console.log("upper left on");
-    offUpperLeftHexa(row, col);
-    toggled = true;
-  }
-  if (checkUpperHexa(row, col)) {
-    console.log("upper on");
-    offUpperHexa(row, col);
-    toggled = true;
-  }
-  if (!toggled) {
-    event.target.classList.add("owned");
+  const hexaList = [];
+  for (var r = row; r <= row + 1; r++) {
+    for (var c = col - 1; c <= col + 1; c++) {
+      var cali_c = c + (r == row ? 0 : getDownColDif(row));
+      console.log("c", r, cali_c);
+      if (checkHexa(r, cali_c)) {
+        console.log(r, cali_c);
+        offHexa(r, cali_c);
+        hexaList.push([r, cali_c]);
+      }
+    }
   }
 }
 function setBoardSize() {
