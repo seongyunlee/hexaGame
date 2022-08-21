10538897b83ba42966f234deb544a23405918f45
seongyunlee <priavate.seongyun@gmail.com>
2022-08-22 00:46:54 +0900

    start btn setting

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const path = require("path");
+const { off } = require("process");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -12,8 +13,110 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
+let numUsers = 0;
+var cells = [];
+var players = [null, null, null, null, null, null];
+var turn = null;
+let isGameStart = false;
+
+const initCells = () => {
+  cells = [];
+  const cnt = [11, 13, 15, 17, 19, 19, 17, 15, 13, 11];
+  for (var r = 0; r < 10; r++) {
+    var line = [];
+    for (var i = 0; i < cnt[r]; i++) {
+      line.push(false);
+    }
+    cells.push(line);
+  }
+};
+const getGameInfo = () => {
+  return [isGameStart, players, turn];
+};
+const initGame = () => {
+  initCells();
+  players = [false, false, false, false, false, false];
+  gameStart = true;
+};
+const getUpperColDif = (row) => {
+  if (row == 5) return 0;
+  if (row > 5) return 1;
+  if (row < 5) return -1;
+};
+const getDownColDif = (row) => {
+  if (row == 4) return 0;
+  if (row > 4) return -1;
+  if (row < 4) return 1;
+};
+const checkHexa = (row, col) => {
+  //upper Hexa //the origin is lower center
+  if (
+    row <= 0 ||
+    row >= cells.length ||
+    col < 0 ||
+    col >= cells[row].length - 1
+  )
+    return false;
+  if (((row > 4) + col) % 2 != 0) return false; // not checking for down cell
+  for (var r = row - 1; r <= row; r++) {
+    for (var c = col - 1; c <= col + 1; c++) {
+      if (!cells[r][c + (r == row ? 0 : getUpperColDif(row))]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const offHexa = (row, col) => {
+  for (var r = row - 1; r <= row; r++) {
+    for (var c = col - 1; c <= col + 1; c++) {
+      cells[r][c + (r == row ? 0 : getUpperColDif(row))] = false;
+    }
+  }
+};
+const checkHexaAround = (row, col) => {
+  const onHexaList = [];
+  for (var r = row; r <= row + 1; r++) {
+    for (var c = col - 1; c <= col + 1; c++) {
+      var cali_c = c + (r == row ? 0 : getDownColDif(row));
+      if (checkHexa(r, cali_c)) {
+        onHexaList.push([r, cali_c]);
+      }
+    }
+  }
+  onHexaList.map(([r, c]) => {
+    offHexa(r, c);
+  });
+};
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  numUsers++;
+  console.log("user connected", numUsers);
+  if (numUsers == 1) {
+    initCells();
+  }
+  io.emit("game_info", getGameInfo());
+  io.emit("board_status", cells);
+  socket.on("click_cell", (data) => {
+    const [r, c] = data.split(" ").map((e) => parseInt(e));
+    cells[r][c] = !cells[r][c];
+    checkHexaAround(r, c);
+    io.emit("board_status", cells);
+  });
+  socket.on("disconnect", () => {
+    --numUsers;
+  });
+  socket.on("start_game", () => {
+    initGame();
+    io.emit("game_info", getGameInfo());
+  });
+  socket.on("take_seat", (data) => {
+    console.log("setat", data - 1);
+    if (players[data - 1] != null) return;
+    players[data - 1] = true;
+    socket.mySeat = data - 1;
+    console.log(getGameInfo());
+    io.emit("game_info", getGameInfo());
+  });
 });
 
 var port = 80;
--- public/main.js
+++ public/main.js
@@ -1,76 +1,38 @@
-var all_tri = [];
+var cells = [];
+const cell_colors = [
+  "#ffffd9",
+  "#ffdc00",
+  "#09ff5e",
+  "#fb4f0e",
+  "#21c2fe",
+  "#77788b",
+];
+const socket = io();
 
-addEventListener("resize", setBoardSize);
-addEventListener("resize", setRootFontSize);
-setBoardSize();
-setBoard();
-setRootFontSize();
-function onMouseOverTri(event) {
+const onClickSeat = (seat_id) => {
+  socket.emit("take_seat", seat_id.slice(-1));
+};
+const setSeat = () => {
+  const seats = document.querySelectorAll("player-seat");
+  for (var i = 0; i < seats.length; i++) {
+    seats[i].addEventListener("click", (e) =>
+      onClickSeat(e.target.getAttribute("id"))
+    );
+  }
+};
+const onClickStart = () => {
+  const start_btn = document.querySelector(".start_btn");
+  start_btn.addEventListener("click", () => socket.emit("start_game"));
+};
+function onMouseOverCell(event) {
   event.target.classList.add("mouse-up");
 }
-function onMouseOutTri(event) {
+function onMouseOutCell(event) {
   event.target.classList.remove("mouse-up");
 }
-function getUpperColDif(row) {
-  if (row == 5) return 0;
-  if (row > 5) return 1;
-  if (row < 5) return -1;
-}
-function getDownColDif(row) {
-  if (row == 4) return 0;
-  if (row > 4) return -1;
-  if (row < 4) return 1;
-}
-
-function checkHexa(row, col) {
-  //upper Hexa //the origin is lower center
-  if (row <= 0 || col >= all_tri[row].length - 1 || col < 0) return false;
-  if (all_tri[row][col].classList.contains("down")) return false;
-  for (var r = row - 1; r <= row; r++) {
-    for (var c = col - 1; c <= col + 1; c++) {
-      if (
-        !all_tri[r][
-          c + (r == row ? 0 : getUpperColDif(row))
-        ].classList.contains("owned")
-      ) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-function offHexa(row, col) {
-  //upper Hexa //the origin is lower center
-  if (all_tri[row][col].classList.contains("down")) return false;
-  for (var r = row - 1; r <= row; r++) {
-    for (var c = col - 1; c <= col + 1; c++) {
-      all_tri[r][c + (r == row ? 0 : getUpperColDif(row))].classList.remove(
-        "owned"
-      );
-    }
-  }
-}
-function onClickTri(event) {
-  event.target.classList.add("owned");
-  const [row, col] = event.target
-    .getAttribute("value")
-    .split(" ")
-    .map((e) => {
-      return parseInt(e);
-    });
-  const hexaList = [];
-  for (var r = row; r <= row + 1; r++) {
-    for (var c = col - 1; c <= col + 1; c++) {
-      var cali_c = c + (r == row ? 0 : getDownColDif(row));
-      console.log("c", r, cali_c);
-      if (checkHexa(r, cali_c)) {
-        console.log(r, cali_c);
-        offHexa(r, cali_c);
-        hexaList.push([r, cali_c]);
-      }
-    }
-  }
-}
+const onClickCell = (event) => {
+  socket.emit("click_cell", event.target.getAttribute("value"));
+};
 function setBoardSize() {
   var board = document.querySelector(".fixed-ratio");
   board.style.width = `${Math.round(window.innerWidth)}px`;
@@ -83,31 +45,70 @@ function setRootFontSize() {
 function setBoard() {
   var board = document.querySelector(".board");
   for (var i = 0; i < 10; i++) {
-    board.appendChild(makeTriangleLine(i));
+    board.appendChild(makeBoardLine(i));
   }
 }
-function makeTriangleLine(idx) {
+function makeBoardLine(idx) {
   const cnt = [11, 13, 15, 17, 19, 19, 17, 15, 13, 11];
   var line = document.createElement("div");
+  var cell_line = [];
   line.setAttribute("class", "triangle-line");
-  var triangles = [];
   for (var i = 0; i < cnt[idx]; i++) {
-    var triangle = document.createElement("div");
-    triangle.setAttribute(
+    var cell = document.createElement("div");
+    cell.setAttribute(
       "class",
       ((idx > 4) + i) % 2 == 0 ? "triangle up" : "triangle down"
     );
-    triangle.addEventListener("mouseover", (e) => onMouseOverTri(e));
-    triangle.addEventListener("mouseout", (e) => onMouseOutTri(e));
-    triangle.addEventListener("click", (e) => onClickTri(e));
-    //triangle.setAttribute("position", "relative");
-    //triangle.setAttribute("top", `${5 * i * l}rem`);
-    triangles.push(triangle);
+    cell.addEventListener("mouseover", (e) => onMouseOverCell(e));
+    cell.addEventListener("mouseout", (e) => onMouseOutCell(e));
+    cell.addEventListener("click", (e) => onClickCell(e));
+    cell.setAttribute("value", `${idx} ${i}`);
+    line.appendChild(cell);
+    cell_line.push(cell);
   }
-  all_tri.push(triangles);
-  for (var i = 0; i < triangles.length; i++) {
-    triangles[i].setAttribute("value", `${idx} ${i}`);
-    line.appendChild(triangles[i]);
-  }
+  cells.push(cell_line);
   return line;
 }
+
+const setSeatPlayer = (players) => {
+  console.log(players);
+  players.map((seated, idx) => {
+    if (seated) {
+      const seat = document.querySelector(`player-seat#box_${idx + 1}`);
+      seat.innerHTML = "SEATED";
+    }
+  });
+};
+const setGameStartBtn = (isGameStart, players) => {
+  const startBtn = document.querySelector(".start_btn");
+  startBtn.style.visibility =
+    !isGameStart && players.filter((e) => e == true).length > 1
+      ? "visible"
+      : "hidden";
+};
+const setGameInfo = ([isGameStart, players, turn]) => {
+  setSeatPlayer(players);
+  setGameStartBtn(isGameStart, players);
+};
+socket.on("board_status", (data) => {
+  for (var r = 0; r < data.length; r++) {
+    for (var c = 0; c < data[r].length; c++) {
+      if (data[r][c]) {
+        cells[r][c].classList.add("owned");
+      } else {
+        cells[r][c].classList.remove("owned");
+      }
+    }
+  }
+});
+socket.on("game_info", (data) => {
+  console.log(data);
+  setGameInfo(data);
+});
+setRootFontSize();
+setBoardSize();
+setBoard();
+setSeat();
+setGameStartBtn();
+addEventListener("resize", setBoardSize);
+addEventListener("resize", setRootFontSize);
